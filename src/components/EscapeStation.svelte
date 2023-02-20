c07c183a8983b23f65bf0a32145d89823674f2fb
wemogy Mac 1 <76742318+bennetrr@users.noreply.github.com>
2023-02-20 21:53:30 +0100

    Improved Styles

--- src/components/EscapeStation.svelte
+++ src/components/EscapeStation.svelte
@@ -26,70 +26,63 @@
     async function submitModal() {
         if ($currentUser.username !== station.name) {
             if (secret !== station.code) {
-                addNotification({type: "warning", text: "You've entered the wrong secret!", duration: 10});
+                addNotification({type: "warning", text: "You've entered the wrong secret!", duration: 5});
                 return;
             }
         }
 
         const userResult = await pb.collection("users").getOne<UsersResponse>($currentUser.id);
         const completedStations = [...userResult.completed_stations, station.id];
-        await pb.collection("users").update<EscapeStationsRecord>($currentUser.id, {...userResult, completed_stations: completedStations});
+        await pb.collection("users")
+            .update<EscapeStationsRecord>($currentUser.id, {...userResult, completed_stations: completedStations});
 
         closeModal();
     }
 </script>
 
 <div class="station-container" class:station-container-complete={station.completed} on:click={openModal}>
-    <span>{station.name}</span>
+    <span class="station-name">{station.name}</span>
 </div>
 
 <Modal on:close={closeModal} opened={modalOpen} target={'body'} title="{station.name}">
     {#if $currentUser.username === station.name}
         <span>
-            This station is yours!
+            This station is yours!<br/>
+            You can claim it without entering the secret.
         </span>
-        <Group override={{marginTop: 15}} position="center">
-            <Button color="teal" on:click={submitModal} override={{width: "7em"}}>
-                Claim it
-            </Button>
-            <Button color="red" on:click={closeModal} override={{width: "7em"}}>
-                Abort
-            </Button>
-        </Group>
     {:else}
         <TextInput bind:value={secret} label="Enter the secret for {station.name}" type="password">
             <svelte:fragment slot="rightSection">
                 <Icon icon={faKey}/>
             </svelte:fragment>
         </TextInput>
-
-        <Group override={{marginTop: 15}} position="center">
-            <Button color="teal" on:click={submitModal} override={{width: "7em"}}>
-                Submit
-            </Button>
-            <Button color="red" on:click={closeModal} override={{width: "7em"}}>
-                Abort
-            </Button>
-        </Group>
     {/if}
+
+    <Group override={{marginTop: 15}} position="center">
+        <Button color="teal" on:click={submitModal} override={{width: "7em"}}>
+            Claim it
+        </Button>
+        <Button color="red" on:click={closeModal} override={{width: "7em"}}>
+            Abort
+        </Button>
+    </Group>
 </Modal>
 
 <style lang="scss">
   @import "../vars";
 
   .station-container {
-    background-color: $base-color-alt1;
-    height: calc(100vh / 2);
     padding: $base-spacing;
-    border: $base-border;
-
     display: flex;
     justify-content: center;
     align-items: center;
 
-    span {
+    background-color: $base-color-alt1;
+    border: $base-border;
+  }
+
+  .station-name {
       font-size: 24px;
-    }
   }
 
   .station-container-complete {
