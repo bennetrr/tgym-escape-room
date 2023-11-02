266a028db751a0e8c82c4666185df64b01932628
Bennet Ranft <bennet@bennetr.me>
2023-11-02 12:38:50 +0100

    Update PocketBase.ts

--- src/connectors/PocketBase.ts
+++ src/connectors/PocketBase.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 import {writable} from "svelte/store";
 import type {UsersResponse} from "../interfaces/PocketBaseTypes";
 
-export const pb = new PocketBase("http://localhost:3457");
+export const pb = new PocketBase("http://localhost:3456");
 // @ts-ignore
 export const currentUser = writable<UsersResponse>(pb.authStore.model);
 
